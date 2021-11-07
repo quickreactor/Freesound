6c51717ff984dbe3e62132ca2ba2aba31c7d47d3
Barnaby Fredric <quickreactor@gmail.com>
2021-11-08 11:41:05 +1300

    history and replay

--- freesound.js
+++ freesound.js
@@ -1,6 +1,16 @@
-const api = "https://freesound.org/apiv2/search/text/?query="
-const fields = "&page_size=40&sort=score&fields=description,previews,duration"
-const token = "&token=ho6JWkyWgzhYRLzctaX7I77ThnsfDkvviuoarmxJ"
+//const api = "https://freesound.org/apiv2/search/text/?query="
+//const fields = "&group_by_pack=1&page_size=40&sort=score&fields=description,previews,duration"
+//const token = "&token=ho6JWkyWgzhYRLzctaX7I77ThnsfDkvviuoarmxJ"
+
+const api = new URL("https://freesound.org/apiv2/search/text/?query=");
+// Equals "?breed=aegan&categories=sinks%2Cspace"
+let paramObject = {
+    group_by_pack: 1,
+    page_size: 40,
+    sort: "score",
+    fields: "description,previews,duration",
+    token: "ho6JWkyWgzhYRLzctaX7I77ThnsfDkvviuoarmxJ"
+}
 
 
 var lastData = {};
@@ -10,18 +20,20 @@ var lastQuery = "";
 function search() {
     var searchInput = document.getElementById("search");
     var query = searchInput.value;
+    paramObject.query = query;
+    api.search = new URLSearchParams(paramObject);
     if (query !== lastQuery) {
-        var requestURL = api + query + fields + token
-        fetch(requestURL)
+        //var requestURL = api + query + fields + token
+        fetch(api)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             lastData = data;
             clearHistory();
-            addToHistory(playRandom(data));
+            addToHistory(play(data));
         });
     } else if (query !== "") {
-        addToHistory(playRandom(lastData));
+        addToHistory(play(lastData));
     }
     lastQuery = query;
 }
@@ -31,10 +43,13 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-function addToHistory(description) {
+function addToHistory(details) {
     let histList = document.getElementById("history");
     let newLi = document.createElement("li");
-    newLi.innerText = description;
+    let newSpan = document.createElement("span");
+    newSpan.innerText = details.number + ". ";
+    newLi.appendChild(newSpan);
+    newLi.innerHTML += details.description;
     histList.appendChild(newLi);
 }
 
@@ -43,13 +58,28 @@ function clearHistory() {
     histList.innerHTML = "";
 }
 
-function playRandom(data) {
-    random = randomIntFromInterval(0, data.results.length-1);
-    console.log(random, data.results[random].description);
-    var previewSrc = data.results[random].previews['preview-hq-mp3'];
+function play(data, number=false) {
+    if (number) {
+
+    } else {
+        number = randomIntFromInterval(0, data.results.length-1);
+        console.log(number, data.results[number].description);
+    }
+    var previewSrc = data.results[number].previews['preview-hq-mp3'];
     var audio = document.getElementById("player");
     audio.src = previewSrc;
     audio.load();
     audio.play();
-    return data.results[random].description;
+    return {
+        description: data.results[number].description,
+        number: number
+    }
+}
+
+
+function replay(event) {
+    let thing = event.target
+    console.log(thing);
+    let number = thing.innerText.split(".")[0];
+    play(lastData, number)
 }
